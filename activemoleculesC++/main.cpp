f63d747d3f0f1f49a09950b580b942083214d99d
yaric_om <yaric_om@localhost>
2015-01-09 11:43:31 +0200

    #1

--- activemoleculesC++/main.cpp
+++ activemoleculesC++/main.cpp
@@ -54,7 +54,7 @@ using namespace std;
 #define VS          VC < string >
 #define VVS         VC < VS >
 
-template<class T> string i2s(T x) {ostringstream o; o << x; return o.str();}
+
 VS splt(string s, char c = ',') {
     VS all;
     int p = 0, np;
@@ -77,68 +77,6 @@ double getTime() {
     gettimeofday(&tv, NULL);
     return tv.tv_sec + 1e-6 * tv.tv_usec;
 }
-
-struct RNG {
-    unsigned int MT[624];
-    int index;
-    
-    RNG(int seed = 1) {
-        init(seed);
-    }
-    
-    void init(int seed = 1) {
-        MT[0] = seed;
-        FOR(i, 1, 624) MT[i] = (1812433253UL * (MT[i-1] ^ (MT[i-1] >> 30)) + i);
-        index = 0;
-    }
-    
-    void generate() {
-        const unsigned int MULT[] = {0, 2567483615UL};
-        REP(i, 227) {
-            unsigned int y = (MT[i] & 0x8000000UL) + (MT[i+1] & 0x7FFFFFFFUL);
-            MT[i] = MT[i+397] ^ (y >> 1);
-            MT[i] ^= MULT[y&1];
-        }
-        FOR(i, 227, 623) {
-            unsigned int y = (MT[i] & 0x8000000UL) + (MT[i+1] & 0x7FFFFFFFUL);
-            MT[i] = MT[i-227] ^ (y >> 1);
-            MT[i] ^= MULT[y&1];
-        }
-        unsigned int y = (MT[623] & 0x8000000UL) + (MT[0] & 0x7FFFFFFFUL);
-        MT[623] = MT[623-227] ^ (y >> 1);
-        MT[623] ^= MULT[y&1];
-    }
-    
-    unsigned int rand() {
-        if (index == 0) {
-            generate();
-        }
-        
-        unsigned int y = MT[index];
-        y ^= y >> 11;
-        y ^= y << 7  & 2636928640UL;
-        y ^= y << 15 & 4022730752UL;
-        y ^= y >> 18;
-        index = index == 623 ? 0 : index + 1;
-        return y;
-    }
-    
-    INLINE int next() {
-        return rand();
-    }
-    
-    INLINE int next(int x) {
-        return rand() % x;
-    }
-    
-    INLINE int next(int a, int b) {
-        return a + (rand() % (b - a));
-    }
-    
-    INLINE double nextDouble() {
-        return (rand() + 0.5) * (1.0 / 4294967296.0);
-    }
-};
 
 struct Entry {
     static constexpr double NVL = -1000;
